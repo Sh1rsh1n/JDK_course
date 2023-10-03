71b95a05aa6b27776b2aefd835934580ae18a6b4
Sh1rsh1n <a.s.shirshin@mail.ru>
2023-10-03 23:25:14 +0300

    added client app commit #003

---
+++ java/src/main/seminar_1/chat/client/ChatClient.java
@@ -0,0 +1,178 @@
+package src.main.seminar_1.chat.client;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ChatClient extends JFrame {
+
+    private final static String LOG_FILE = "log.txt";
+
+    private static int WINDOW_X = 1000;
+    private static int WINDOW_Y = 500;
+    private static int WINDOW_HEIGHT = 500;
+    private static int WINDOW_WIGHT = 500;
+
+    // верхние панели настроек входа и подключения
+    private JPanel serverSettingsPanel = new JPanel(new GridLayout(1, 3));
+    private JPanel userLoginPanel = new JPanel(new GridLayout(1, 3));
+    private JPanel headerPanel = new JPanel(new GridLayout(2, 1));
+
+    // компоненты верхней панели
+    private JTextField address = new JTextField("127.0.0.1");
+    private JTextField port = new JTextField("241");
+    private JTextField login = new JTextField();
+    private JPasswordField password = new JPasswordField();
+    private JButton loginBtn = new JButton("login");
+
+    // область текста
+    private JTextArea log = new JTextArea();
+
+    // панель отправки сообщения
+    private JPanel footerPanel = new JPanel(new BorderLayout());
+    private JTextField messageField = new JTextField();
+    private JButton sendBtn = new JButton("Отправить");
+
+    private JList<String> jList = new JList<>();
+
+    public ChatClient() {
+
+        try {
+            if (!Files.exists(Path.of(LOG_FILE)))
+                Files.createFile(Path.of(LOG_FILE));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        // параметры основного окна
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocation(WINDOW_X, WINDOW_Y);
+        setSize(WINDOW_WIGHT, WINDOW_HEIGHT);
+        setTitle("Simple Chat Client");
+
+        // верхняя панель настроек подключения
+        serverSettingsPanel.add(address);
+        serverSettingsPanel.add(port);
+        userLoginPanel.add(login);
+        userLoginPanel.add(password);
+        userLoginPanel.add(loginBtn);
+
+        headerPanel.add(serverSettingsPanel);
+        headerPanel.add(userLoginPanel);
+
+        add(headerPanel, BorderLayout.NORTH);
+
+        //центральная област отображения текста
+        log.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(log);
+        add(scrollPane);
+        scrollPane.setVisible(false);
+
+        footerPanel.add(messageField, BorderLayout.CENTER);
+        footerPanel.add(sendBtn, BorderLayout.EAST);
+        footerPanel.setVisible(false);
+        add(footerPanel, BorderLayout.SOUTH);
+
+        String[] userArray = new String[]{"Alex", "Pavel", "Anna"};
+        jList.setListData(userArray);
+        jList.setVisible(false);
+        add(jList, BorderLayout.WEST);
+
+        setVisible(true);
+
+        loginBtn.addActionListener(e -> {
+            if (!checkLogin(login.getText(), password.getText())) {
+                JOptionPane.showMessageDialog(this,
+                        "Неверный логин или пароль",
+                        "Ошибка авторизации.", JOptionPane.ERROR_MESSAGE);
+            } else {
+
+                try (BufferedReader br = new BufferedReader(new FileReader(LOG_FILE))) {
+                    String str;
+                    while ((str = br.readLine()) != null) {
+                        log.append(str);
+                        log.append("\n");
+                    }
+                }catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                jList.setVisible(true);
+                footerPanel.setVisible(true);
+                scrollPane.setVisible(true);
+                loginBtn.setEnabled(false);
+            }
+        });
+
+        // отправка сообщения по нажатию клавиши ENTER
+        messageField.addKeyListener(new KeyAdapter() {
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    log.append(String.format("%s %s %s\n",
+                            jList.getSelectedValue(),
+                            messageField.getText(),
+                            new Date()));
+
+                    try (BufferedWriter bw = new BufferedWriter(new FileWriter(LOG_FILE, true))) {
+                        bw.append(String.format("%s %s %s\n", jList.getSelectedValue(), messageField.getText(), new Date()));
+                        bw.flush();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+
+                    messageField.setText("");
+                }
+
+
+            }
+        });
+
+        // отправка сообщения в чат
+        sendBtn.addActionListener(e -> {
+            log.append(String.format("%s %s %s\n",
+                    jList.getSelectedValue(),
+                    messageField.getText(),
+                    new Date()));
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(LOG_FILE, true))) {
+                bw.append(String.format("%s %s %s\n", jList.getSelectedValue(), messageField.getText(), new Date()));
+                bw.flush();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            messageField.setText("");
+        });
+    }
+
+    // типа проверка входа пользователя
+    private boolean checkLogin(String login, String password) {
+        for (Map.Entry<String, String> entry : userLoginDataBase().entrySet()) {
+            if (login.equalsIgnoreCase(entry.getKey()) && password.equals(entry.getValue())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // типа БД пользователей
+    private Map<String, String> userLoginDataBase() {
+        Map<String, String> map = new HashMap<>();
+
+        map.put("alex", "123qwe");
+        map.put("den", "123qwe");
+
+        return map;
+    }
+}
---
+++ java/src/main/seminar_1/chat/client/MainAppClient.java
@@ -0,0 +1,7 @@
+package src.main.seminar_1.chat.client;
+
+public class MainAppClient {
+    public static void main(String[] args) {
+        new ChatClient();
+    }
+}
