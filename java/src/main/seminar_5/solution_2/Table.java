2a37c29082787a88716a36aa894f6bb2d80e09a7
dev_hp_laptop <a.s.shirshin@mail.ru>
2023-10-30 22:31:11 +0300

    seminar_5 commit #009 solution #2

--- java/src/main/seminar_5/solution_2/Table.java
+++ java/src/main/seminar_5/solution_2/Table.java
@@ -13,47 +13,49 @@ import java.util.concurrent.Semaphore;
  * Класс, Table выполняет инициализацию философов и вилок
  * метод init() - определяет порядок расположения вилок между философами
  * метод start() - запускает приложение
+ * @semaphore - глобальный параметр, чтобы в приложении одновременно могло работать только два потока
  */
 public class Table {
 
-  private Philosopher[] philosophers = new Philosopher[5];
-  private Fork[] forks = new Fork[5];
-  private Semaphore semaphore;
+    private Philosopher[] philosophers = new Philosopher[5];
+    private Fork[] forks = new Fork[5];
+    private Semaphore semaphore;
 
-  public Table() {
-    semaphore = new Semaphore(2);
-    init();
-  }
-
-  /**
-   * Запуск потоков с философами, у которых вызываем метод eat()
-   */
-  public void start() {
-    for (int i = 0; i < philosophers.length; i++) {
-      philosophers[i].start();
-    }
-  }
-
-  /**
-   * Метод инициализации, "рассаживаем философов за стол и раскладываем вилки
-   * между ними"
-   */
-  private void init() {
-    // forks init
-    for (int i = 0; i < forks.length; i++) {
-      forks[i] = new Fork();
+    public Table() {
+        semaphore = new Semaphore(2);
+        init();
     }
 
-    System.out.println("Вилки разложены");
+    /**
+     * Запуск потоков-философов
+     */
+    public void start() {
+        for (int i = 0; i < philosophers.length; i++) {
+            philosophers[i].start();
+        }
+    }
 
-    // philosophers init
-    philosophers[0] = new Philosopher("Платон", forks[4], forks[0], semaphore);
-    philosophers[1] = new Philosopher("Аристотель", forks[0], forks[1], semaphore);
-    philosophers[2] = new Philosopher("Гераклит", forks[1], forks[2], semaphore);
-    philosophers[3] = new Philosopher("Демокрит", forks[2], forks[3], semaphore);
-    philosophers[4] = new Philosopher("Гиппократ", forks[3], forks[4], semaphore);
+    /**
+     * Метод инициализации, "рассаживаем философов за стол и раскладываем вилки
+     * между ними"
+     */
+    private void init() {
+        // forks init
+        for (int i = 0; i < forks.length; i++) {
+            forks[i] = new Fork();
+        }
 
-    System.out.println("Философы уселись за стол");
-  }
+        System.out.println("Вилки разложены");
+
+        // philosophers init
+        philosophers[0] = new Philosopher("Платон", forks[4], forks[0], semaphore);
+        philosophers[1] = new Philosopher("Аристотель", forks[0], forks[1], semaphore);
+        philosophers[2] = new Philosopher("Гераклит", forks[1], forks[2], semaphore);
+        philosophers[3] = new Philosopher("Демокрит", forks[2], forks[3], semaphore);
+        philosophers[4] = new Philosopher("Гиппократ", forks[3], forks[4], semaphore);
+
+        System.out.println("Философы уселись за стол");
+        System.out.println("========================");
+    }
 
 }
--- java/src/main/seminar_5/solution_2/model/Philosopher.java
+++ java/src/main/seminar_5/solution_2/model/Philosopher.java
@@ -1,106 +1,104 @@
 package src.main.seminar_5.solution_2.model;
 
 import java.util.Random;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
 
 /**
  * Класс, Философ
- * Параметры:
  *
  * @name - имя
  * @leftFork - вилка слева от философа
  * @rigthFork - вилка справа от философа
  * @satiety - сытости философа
+ * @semaphore
  */
 public class Philosopher extends Thread {
 
-  private final String name;
-  private final Fork leftFork;
-  private final Fork rightFork;
-  private int satiety;
-  private Semaphore semaphor;
+    private final String name;
+    private final Fork leftFork;
+    private final Fork rightFork;
+    private int satiety;
+    private Semaphore semaphore;
 
-  public Philosopher(String name, Fork leftFork, Fork rightFork, Semaphore semaphore) {
-    this.name = name;
-    this.rightFork = rightFork;
-    this.leftFork = leftFork;
-    this.semaphor = semaphore;
-  }
+    public Philosopher(String name, Fork leftFork, Fork rightFork, Semaphore semaphore) {
+        this.name = name;
+        this.rightFork = rightFork;
+        this.leftFork = leftFork;
+        this.semaphore = semaphore;
+    }
 
-  /**
-   * Метод имитации процесса приема пищи философом
-   * проверяет доступность вилок справа, слева и сытость философа
-   * далее, меняет доступность вилок и засыпает на 5 секунд, после вызывает метод
-   * размышления(think())
-   */
-  private void eat() {
-    Random rnd = new Random();
-    try {
-      while (satiety < 3) {
-        if (leftFork.tryLock()) {
-          System.out.printf("Философ %s взял левую вилку\n", name);
-          if (rightFork.tryLock()) {
-            semaphor.acquire();
-            System.out.printf("Философ %s взял правую вилку\n", name);
+    /**
+     * Метод имитации процесса приема пищи философом
+     * пытается заблокировать вначале левую от себя вилку, а затем правую
+     * в случае успеха, меняет значение семафора
+     * все это выполняется в цикле, пока философ не наестся (параметр satiety < 3)
+     *
+     * если правую вилку не получилось захватить, то отпускает левую (чтобы не было взаимной блокировки)
+     * так же освобождает семафор
+     */
+    private void eat() {
+        try {
+            while (satiety < 3) {
+                if (leftFork.tryLock()) {
+                    System.out.printf("Философ %s взял левую вилку\n", name);
+                    if (rightFork.tryLock()) {
+                        semaphore.acquire();
+                        System.out.printf("Философ %s взял правую вилку\n", name);
 
-            System.out.printf("У Философа %s теперь обе вилки # %s и # %s и он может поесть\n", name,
-                leftFork.getForkNo(), rightFork.getForkNo());
-            
-            int msec = rnd.nextInt(10000);
+                        System.out.printf("У философа %s теперь обе вилки # %s и # %s и он может поесть\n",
+                                name, leftFork.getForkNo(), rightFork.getForkNo());
 
-            System.out.println(name + " кушает " + msec);
+                        System.out.println(name + " кушает.");
 
-            Thread.sleep(msec);
+                        Thread.sleep(5000);
 
-            satiety++;
+                        satiety++;
 
-            System.out.printf("Философ %s наелся. Он поел уже %d\n", name, satiety);
+                        System.out.printf("Философ %s наелся. Он поел уже %d\n", name, satiety);
 
-            think();
+                        think();
 
-          } else {
-            leftFork.unlock();
-            semaphor.release();
-            System.out.println(name + " положил левую вилку, так как не смог взять правую.");
-            try {
-              Thread.sleep(5000);
-            } catch (InterruptedException e) {
-              e.printStackTrace();
+                    } else {
+                        leftFork.unlock();
+                        semaphore.release();
+                        System.out.println(name + " положил левую вилку, так как не смог взять правую.");
+                        try {
+                            Thread.sleep(5000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
             }
-          }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-      }
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
     }
-  }
 
-  /**
-   * Метод, размышления
-   * меняет состояние вилок на "доступны" (true)
-   * засыпает на 5 секунд
-   */
-  private void think() {
-    
-    System.out.printf("Философ %s думает.\n", name);
+    /**
+     * Метод, размышления
+     * разблокирует вилки
+     * освобождает семафор
+     * засыпает на 5 секунд
+     */
+    private void think() {
 
-    semaphor.release();
+        System.out.printf("Философ %s думает.\n", name);
 
-    leftFork.unlock();
-    rightFork.unlock();
+        semaphore.release();
 
-    try {
-      Thread.sleep(5000);
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
+        leftFork.unlock();
+        rightFork.unlock();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
-  }
 
-  @Override
-  public void run() {
-    eat();
-  }
+    @Override
+    public void run() {
+        eat();
+    }
 }
