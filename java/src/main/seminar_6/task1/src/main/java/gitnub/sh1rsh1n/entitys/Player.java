92db3c5f95d1c70d6b438d4d3f391c1d61ed5dce
Sh1rsh1n <95686701+Sh1rsh1n@users.noreply.github.com>
2023-11-16 11:03:01 +0000

    seminar_6 commit #003

--- java/src/main/seminar_6/task1/src/main/java/gitnub/sh1rsh1n/entitys/Player.java
+++ java/src/main/seminar_6/task1/src/main/java/gitnub/sh1rsh1n/entitys/Player.java
@@ -2,4 +2,23 @@ package main.java.gitnub.sh1rsh1n.entitys;
 
 public class Player {
     
+    private CardBroker broker;
+    
+
+    public CardBroker getBroker() {
+        return broker;
+    }
+
+    public void setBroker(CardBroker broker) {
+        this.broker = broker;
+    }
+
+    public Player(CardBroker broker) {
+        this.broker = broker;
+        
+    }
+
+    public void choiceCard(){
+        broker.
+    }
 }
